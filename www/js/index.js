2e9dc99bf002d1fa3d5cee02eca8aedf31c4e41f
masbirin <36230649+masbirin@users.noreply.github.com>
2018-05-28 17:57:41 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=Nc0wKUkEbAWFwiajBYMFrIjlJdUUG1lFRHcBps8G";
         
         $("#loadImage").click(function(){
             $.ajax({
